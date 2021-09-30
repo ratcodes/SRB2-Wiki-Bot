a980546324a8cc737ec5aeef826750150cdf03d9
ash <35163546+ashfelix@users.noreply.github.com>
2021-09-29 21:49:42 -0400

    updated repo link, name

--- SRB2WikiBot/Constants.cs
+++ SRB2WikiBot/Constants.cs
@@ -14,10 +14,10 @@ namespace SRB2WikiBot
         /// <summary>The default thumbnail of the SRB2 Wiki.</summary>
         public const string DEFAULT_THUMBNAIL = @"https://wiki.srb2.org/w/images/thumb/7/79/Srb2wiki_logo-big.png/225px-Srb2wiki_logo-big.png";
         /// <summary>The link to the repo of this bot.</summary>
-        public const string REPO_LINK = @"https://github.com/ashfelix";
+        public const string REPO_LINK = @"https://github.com/ashfelix/SRB2-Wiki-Bot/";
         /// <summary>The invite link of the bot.</summary>
         public const string INVITE_LINK = @"https://discord.com/api/oauth2/authorize?client_id=884526800603054181&permissions=2048&scope=bot";
-        /// <summary>The default URL path to the SRB2 Wiki..</summary>
+        /// <summary>The default URL path to the SRB2 Wiki.</summary>
         public const string WIKI_URL_PATH = @"https://wiki.srb2.org/wiki/";
     }
 }
